aed07c7356eac5c87261d32715c0d71b3bd880b7
JimenaZubaran <jimenab.zubaran@gmail.com>
2018-01-30 10:34:52 -0600

    estilos newsfeed, amigos y fotos. Firebase agregado

--- js/app.js
+++ js/app.js
@@ -1,3 +1,48 @@
+var config = {
+  apiKey: "AIzaSyDgEslLzmClEN53ERhCZn2pQbJC03M-KF4",
+  authDomain: "redlaboratorias.firebaseapp.com",
+  databaseURL: "https://redlaboratorias.firebaseio.com",
+  projectId: "redlaboratorias",
+  storageBucket: "redlaboratorias.appspot.com",
+  messagingSenderId: "791147202268"
+};
+firebase.initializeApp(config);
+
+$("#login-button").click(function(){
+  authGoogle();
+})
+
+function authGoogle(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+  authentication(provider);
+}
+
+function authentication(provider){
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  console.log(result);
+  // ...
+})
+.catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  console.log(errorCode);
+  var errorMessage = error.message;
+  console.log(errorMessage);
+  // The email of the user's account used.
+  var email = error.email;
+  console.log(email);
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  console.log(credential);
+  // ...
+});
+}
+
+/*Funcion para autenticar log in
 var $userNameInput = $("#username-input");
 var $passwordInput = $("#password-input");
 
@@ -5,8 +50,6 @@ function loadPage(){
   $userNameInput.keyup(validateUser);
   $passwordInput.keyup(validateUser);
 }
-
-
 
 function validateUser (){
 var $logInButton = $("#login-button");
@@ -18,15 +61,25 @@ if ($userNameInput.val().trim().lenght > 0 &&  $passwordInput.val().trim().lengh
   }
 }
 
+$("#add-coment").click(function(e){
+  e.preventDefault();
+  var $newComment = $("#new-comment").val();
 
 
+  //DOM para agregar comentario en perfil
+var $commentBox = $("<div />").addClass("comment-box");
+var $commentName = $("<h3 />");
+var $commentParagraph = $("<p />");
 
+//Agregar atributos
+$commentName.text("nombre");
+$commentParagraph.text("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco");
 
+//Agregar párrafo
+$commentBox.append($commentName);
+$commentBox.append($commentParagraph);
 
-
-
-
-
-
-
-$(document).ready(loadPage);
+//Agregar al área de posts
+$("#")
+})
+*/
