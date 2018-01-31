3dc39cd5171b5f71af01916e05ac46e073514767
JimenaZubaran <jimenab.zubaran@gmail.com>
2018-01-31 08:57:54 -0600

    agregado funcion de comentario, estilos y readme

---
+++ README.md
@@ -0,0 +1,39 @@
+
+# Red Social
+
+Un emprendedor muy visionario nos ha encargado crear una red social. No nos da mucho detalle sobre qué tipo de red social quiere, él solo nos dice que creemos la que mejor podamos y que luego lo convenzamos de que nuestra red social será la más exitosa.
+
+## Algunos puntos básicos que debe tener una red social:
+
+* Perfiles de usuarios
+* Un newsfeed donde puedes ver las actualizaciones de todos tus contactos
+* Un lugar donde poder escribir posts
+* Un lugar para subir fotos
+* Capacidad para poder tener amigos o para poder seguir a personas/marcas dentro de la red social
+* Ser mobile friendly
+
+
+### ¿QUÉ SE REQUIERE?
+* Trabajar en parejas
+* Tener una planificación
+* La definición del diseño
+* Diseño mobile friendly
+* Desplegar el producto en Github Pages
+* Opcional, si se desarrolla un login o se desea almacenar la información en una base de datos, usar Firebase.
+
+
+### Pre-requisitos
+
+Requiere explorador web
+```
+Chrome, Safari, Internet explorer...
+```
+
+### <RedLaboratorians>
+Sketch
+![Log in](./assets/images/1.jpg)
+![Newsfeed](./assets/images/2.jpg)
+![Profile](./assets/images/3.jpg)
+![Photos](./assets/images/4.jpg)
+![Friends](./assets/images/5.jpg)
+## < l a b o r a t o r i a >
--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@ function authentication(provider){
   var token = result.credential.accessToken;
   // The signed-in user info.
   var user = result.user;
-  window.location.href = "newsfeed.html"
+  //window.location.href = "./views/newsfeed.html"
   console.log(result);
   // ...
 })
@@ -47,27 +47,6 @@ function authentication(provider){
 
 
 
-/*
-//Funcion para autenticar log in
-var $userNameInput = $("#username-input");
-var $passwordInput = $("#password-input");
-
-function loadPage(){
-  $userNameInput.keyup(validateUser);
-  $passwordInput.keyup(validateUser);
-}
-
-function validateUser (){
-var $logInButton = $("#login-button");
-
-if ($userNameInput.val().trim().lenght > 0 &&  $passwordInput.val().trim().lenght > 0) {
-  $logInButton.removeAttr("disabled");
-  } else {
-    $logInButton.attr("disabled", true);
-  }
-}
-
-*/
 
 //Funcion para gregar comentario en perfil
 $("#add-comment").click(function(e){
@@ -81,19 +60,64 @@ var $commentBox = $("<div />").addClass("comment-box");
 var $commentPicture =$ ("<img />").addClass("comment-picture")
 var $commentName = $("<h3 />");
 var $commentParagraph = $("<p />");
+var $commentIcon = $("<i />").addClass("fa fa-heart comment-icons");
+var $commentInput = $("<input/>").addClass("comment-input");
+var $commentButton = $("<button/>")
+
 
 //Agregar atributos
 $commentPicture.attr("src", "./../assets/images/my-profile.jpg" )
 $commentName.text("Paola Torres");
 $commentParagraph.html(newComment);
+$commentInput.attr("placeholder", "Escribe un comentario");
+$commentButton.attr("id", "comment-add");
+$commentButton.html("Agregar");
 
 //Agregar párrafo
 $commentBox.append($commentPicture);
 $commentBox.append($commentName);
 $commentBox.append($commentParagraph);
+$commentBox.append($commentIcon);
+$commentBox.append($commentInput);
+$commentBox.append($commentButton);
 
 //Agregar al área de posts
 $("#new-posts").prepend($commentBox);
+
+
+  $("#comment-add").click(function(e){
+    e.preventDefault();
+    var commentInput = $(".comment-input").val();
+
+    //DOM para agregar comentario en en el post
+    var $commentsBox = $("<div />").addClass("comments-box");
+    var $commentsPicture =$ ("<img />").addClass("comments-picture")
+    var $commentsName = $("<h3 />");
+    var $commentsParagraph = $("<p />");
+    //var $commentsIcon = $("<i />").addClass("fa fa-heart, comments-icons");
+    var $commentsInput = $("<input/>").addClass("commenst-input");
+    var $commentsButton = $("<button/>")
+
+    //Agregar atributos
+    $commentsPicture.attr("src", "./../assets/images/friend7.jpg" )
+    $commentsName.text("Valentina Sánchez");
+    $commentsParagraph.html(commentInput);
+    $commentsInput.attr("placeholder", "Escribe un comentario");
+    $commentsButton.attr("id", "comments-add");
+    $commentsButton.html("Agregar");
+
+    //Agregar párrafo
+    $commentsBox.append($commentsPicture);
+    $commentsBox.append($commentsName);
+    $commentsBox.append($commentsParagraph);
+    //$commentsBox.append($commentsIcon);
+    $commentsBox.append($commentsInput);
+    $commentsBox.append($commentsButton);
+
+
+    //Agregar al área de posts
+    $(".comment-box").append($commentsBox);
+  });
 });
 
 
